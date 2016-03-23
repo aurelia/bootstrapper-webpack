ff958ad05fe16178b0afbf058a4c7af2ce831406
Dwayne Charrington <dwaynecharrington@gmail.com>
2016-03-23 12:57:59 +1000

    fix(all): issue with configure method on empty object fixed

--- src/index.js
+++ src/index.js
@@ -48,7 +48,11 @@ function config(loader, appHost, configModuleId) {
   aurelia.host = appHost;
 
   if (configModuleId) {
-    return loader.loadModule(configModuleId).then(customConfig => customConfig.configure(aurelia));
+    return loader.loadModule(configModuleId).then(customConfig => {
+      if (customConfig.configure) {
+       return customConfig.configure(aurelia); 
+      }
+    });
   }
 
   aurelia.use
